ba535f129af3543b673a4e84b97f834dc8c89d54
Patrick Lyden <syncc12@verizon.net>
2019-06-14 03:25:53 +0000

    Added sign in requirement to post new places and added poster's email address to their posts

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     # @places = Place.all
     @places = Place.order("places").page(params[:page]).per_page(10)
@@ -10,7 +10,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
